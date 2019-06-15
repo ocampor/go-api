3269330257fa8eeb1285299e4d051916f66f42a4
Ricardo Ocampo <r.ocampo.vega@gmail.com>
2019-06-15 19:48:55 +0400

    Add overall similarity and string similarity function

--- src/models/models.go
+++ src/models/models.go
@@ -4,16 +4,16 @@ type Unit struct {
 	PropertyId int     `json:"property_id" description:"government database property identifier" gorm:"primary_key"`
 	LocationId int     `json:"location_id" description:"property finder unit location identifier"`
 	BedroomId  int     `json:"bedroom_id" description:"property finder bedrooms identifier"`
-	UnitSize   float32 `json:"unit_size" description:"plot area of the property"`
+	UnitSize   float64 `json:"unit_size" description:"plot area of the property"`
 	UnitNumber string  `json:"unit_number" description:"unit number of the property"`
 }
 
 type DetailValidation struct {
-	OverallSimilarity    float32 `json:"overall_similarity" description:"similarity between government property details and parameters"`
+	OverallSimilarity    float64 `json:"overall_similarity" description:"overall similarity between goverment details and listing details"`
 	LocationMatches      bool    `json:"location_matches" description:"the listing is under the same location tree as the government property"`
 	BedroomMatches       bool    `json:"bedroom_matches" description:"the listing bedrooms and the government property matches"`
-	UnitSizeSimilarity   float32 `json:"unit_size_similarity" description:"similarity between government property unit size and listing"`
-	UnitNumberSimilarity float32 `json:"unit_number_similarity" description:"similarity the listing unit number and the government property"`
+	UnitSizeSimilarity   float64 `json:"unit_size_similarity" description:"similarity between government property unit size and listing"`
+	UnitNumberSimilarity float64 `json:"unit_number_similarity" description:"similarity the listing unit number and the government property"`
 }
 
 func (Unit) TableName() string {
--- src/services/units_service.go
+++ src/services/units_service.go
@@ -5,7 +5,10 @@ import (
 	"github.com/emicklei/go-restful"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"github.com/texttheater/golang-levenshtein/levenshtein"
+	"math"
 	"net/http"
+	"strconv"
 )
 
 type UnitResource struct {
@@ -31,7 +34,7 @@ func (u UnitResource) UnitService() *restful.WebService {
 			Param(ws.PathParameter("property_id", "identifier of the property").DataType("integer")).
 			Param(ws.QueryParameter("location_id", "property finder unit location identifier").DataType("integer")).
 			Param(ws.QueryParameter("bedroom_id", "property finder bedrooms identifier").DataType("integer")).
-			Param(ws.QueryParameter("unit_size", "plot area of the property").DataType("float32")).
+			Param(ws.QueryParameter("unit_size", "plot area of the property").DataType("float64")).
 			Param(ws.QueryParameter("unit_number", "unit number of the property").DataType("string")).
 			Writes(models.DetailValidation{}).
 			Returns(http.StatusOK, http.StatusText(http.StatusOK), models.DetailValidation{}).
@@ -52,8 +55,58 @@ func (u UnitResource) findUnit(request *restful.Request, response *restful.Respo
 	}
 }
 
+func floatSimilarity(a float64, b float64) float64 {
+	if a < b {
+		return math.Abs(a / b)
+	} else {
+		return math.Abs(b / a)
+	}
+}
+
+func boolToFloat64(a bool) float64 {
+	if a {
+		return 1.0
+	} else {
+		return 0.0
+	}
+}
+
+func stringSimilarity(a string, b string) float64 {
+	return levenshtein.RatioForStrings([]rune(a), []rune(b), levenshtein.DefaultOptions)
+}
+
 func (u UnitResource) validateDetails(request *restful.Request, response *restful.Response) {
-	var detailValidation = models.DetailValidation{0.9, true, true, 0.84, 0.75}
+	id := request.PathParameter("property_id")
+	locationId, _ := strconv.Atoi(request.QueryParameter("location_id"))
+	bedroomId, _ := strconv.Atoi(request.QueryParameter("bedroom_id"))
+	unitSize, _ := strconv.ParseFloat(request.QueryParameter("unit_size"), 64)
+	unitNumber := request.QueryParameter("unit_number")
+
+	var unit = models.Unit{}
+	u.Db.First(&unit, id)
+
+	if unit.PropertyId == 0 {
+		response.WriteErrorString(http.StatusNotFound, "Unit could not be found")
+	}
+
+	locationMatches := locationId == unit.LocationId
+	bedroomMatches := bedroomId == unit.BedroomId
+
+	unitSizeSimilarity := floatSimilarity(unit.UnitSize, unitSize)
+	unitNumberSimilarity := stringSimilarity(unit.UnitNumber, unitNumber)
+
+	overallSimilarity := (boolToFloat64(locationMatches) +
+		boolToFloat64(bedroomMatches) +
+		unitSizeSimilarity +
+		unitNumberSimilarity) / 4
+
+	var detailValidation = models.DetailValidation{
+		overallSimilarity,
+		locationMatches,
+		bedroomMatches,
+		unitSizeSimilarity,
+		unitNumberSimilarity,
+	}
 
 	response.WriteEntity(detailValidation)
 }
